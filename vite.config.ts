aead9c05658f36226f40ec607a8656a509ffbb35
ultimatefaloe <ultimatefaloe@outlool.com>
2026-02-11 11:38:35 +0100

    setting up production env

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'url'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   resolve: {
@@ -18,10 +19,10 @@ const config = defineConfig({
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
-
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    nitro(),
   ],
 })
 
